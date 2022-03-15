03f38e8da32a240527e583eb13c3fcfa7c65f88e
zhao-feifei <398983914@qq.com>
2022-03-15 19:08:48 +0800

    新增注释

--- observer/index.js
+++ observer/index.js
@@ -18,10 +18,7 @@ import {
 
 const arrayKeys = Object.getOwnPropertyNames(arrayMethods);
 
-/**
- * In some cases we may want to disable observation inside a component's
- * update computation.
- */
+/* 标识，避免重复对value进行Observer */
 export let shouldObserve: boolean = true;
 
 export function toggleObserving(value: boolean) {
@@ -53,7 +50,7 @@ export class Observer {
       this.walk(value);
     }
   }
-
+  /* walk方法传入一个对象，遍历此对象的每一个属性并进行defineReactive绑定 */
   walk(obj: Object) {
     const keys = Object.keys(obj);
     for (let i = 0; i < keys.length; i++) {
@@ -61,9 +58,7 @@ export class Observer {
     }
   }
 
-  /**
-   * Observe a list of Array items.
-   */
+  /* 对数组的每一个成员进行observe */
   observeArray(items: Array<any>) {
     for (let i = 0, l = items.length; i < l; i++) {
       observe(items[i]);
@@ -71,23 +66,11 @@ export class Observer {
   }
 }
 
-// helpers
-
-/**
- * Augment a target Object or Array by intercepting
- * the prototype chain using __proto__
- */
+/* 下面这个两个 */
 function protoAugment(target, src: Object) {
-  /* eslint-disable no-proto */
   target.__proto__ = src;
-  /* eslint-enable no-proto */
 }
 
-/**
- * Augment a target Object or Array by defining
- * hidden properties.
- */
-/* istanbul ignore next */
 function copyAugment(target: Object, src: Object, keys: Array<string>) {
   for (let i = 0, l = keys.length; i < l; i++) {
     const key = keys[i];
@@ -96,24 +79,25 @@ function copyAugment(target: Object, src: Object, keys: Array<string>) {
 }
 
 /**
- * Attempt to create an observer instance for a value,
- * returns the new observer if successfully observed,
- * or the existing observer if the value already has one.
+ * 创建一个Observer实例（__ob__），如果成功创建Observer实例则返回新的Observer实例，如果已有Observer实例则返回现有的Observer实例。
  */
 export function observe(value: any, asRootData: ?boolean): Observer | void {
   if (!isObject(value) || value instanceof VNode) {
     return;
   }
   let ob: Observer | void;
+  /* 通过 __ob__属性判断是否已经有Observer实例*/
   if (hasOwn(value, '__ob__') && value.__ob__ instanceof Observer) {
     ob = value.__ob__;
   } else if (
+    /* 确保value是对象 */
     shouldObserve &&
     !isServerRendering() &&
     (Array.isArray(value) || isPlainObject(value)) &&
     Object.isExtensible(value) &&
     !value._isVue
   ) {
+    /* 如果没有Observer实例就new一个新的 */
     ob = new Observer(value);
   }
   if (asRootData && ob) {
@@ -132,20 +116,22 @@ export function defineReactive(
   customSetter?: ?Function,
   shallow?: boolean
 ) {
+  /* 在闭包中定义dep对象 */
   const dep = new Dep();
 
   const property = Object.getOwnPropertyDescriptor(obj, key);
+  /* 如果没有key属性或者此属性是不可配置的，直接返回*/
   if (property && property.configurable === false) {
     return;
   }
 
-  // cater for pre-defined getter/setters
+  /* 取出预设的getter/setter */
   const getter = property && property.get;
   const setter = property && property.set;
   if ((!getter || setter) && arguments.length === 2) {
     val = obj[key];
   }
-
+  /* 子对象递归进行observe */
   let childOb = !shallow && observe(val);
   Object.defineProperty(obj, key, {
     enumerable: true,
@@ -153,10 +139,13 @@ export function defineReactive(
     get: function reactiveGetter() {
       const value = getter ? getter.call(obj) : val;
       if (Dep.target) {
+        /* 依赖收集 */
         dep.depend();
         if (childOb) {
+          /* 子对象依赖收集 */
           childOb.dep.depend();
           if (Array.isArray(value)) {
+            /* 对数组的每个成员进行依赖收集 */
             dependArray(value);
           }
         }
